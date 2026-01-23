dcc0af02dc4f75f770d4b76e852d052c53988de4
Bohdan.Klymov <programmer.b.k.339@gmail.com>
2026-01-23 14:55:51 +0100

    Started with Java Problem called TwoSum

---
+++ src/TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        nums = new int[] {2, 7, 11, 15};
+        target = 9;
+
+        int[] result = {0, 0};
+        boolean stopPoint = false;
+
+        while (stopPoint != true) {
+            for (int i = 0; i < NotСompletelyNums; i++) {
+                if () {
+
+                }
+            }
+        }
+    }
+}
