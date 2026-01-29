dfb15c7118f5b1b861b2dc024f49118ef51fca06
Bohdan.Klymov <programmer.b.k.339@gmail.com>
2026-01-29 10:07:46 +0100

    Updated final version of TwoSum problem.

--- src/Easy_level_of_problem/TwoSum.java
+++ src/Easy_level_of_problem/TwoSum.java
@@ -1,9 +1,9 @@
-
+package Easy_level_of_problem;
 //Given an array of integers nums and an integer target,
 //return indices of the two numbers such that they add up to target.
 
 public class TwoSum {
-    public static void main(String[] args) {
+    public static int[] main(String[] args) {
         int[] nums = {2, 16, 11, 7};
         int target = 9;
 
@@ -11,10 +11,11 @@ public class TwoSum {
             for (int j = i + 1; j < nums.length; j++) {
 
                 if (nums[i] + nums[j] == target) {
-                    System.out.println(i + " " + j);
-                    return;
+                    int[] result = {i, j};
+                    return result;
                 }
             }
         }
+        return nums;
     }
 }
