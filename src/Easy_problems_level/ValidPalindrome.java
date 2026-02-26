15d2586b661f991efe03b77b3212c6a632165aeb
Bohdan Klymov <programmer.b.k.339@gmail.com>
2026-02-26 05:52:12 +0100

    Made progress with solving this LeetCode problem.

--- src/Easy_problems_level/ValidPalindrome.java
+++ src/Easy_problems_level/ValidPalindrome.java
@@ -1,23 +1,51 @@
 package Easy_problems_level;
 import java.util.Scanner;
+import java.util.ArrayList;
+
+//  A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and
+//  removing all non-alphanumeric characters, it reads the same forward and backward.
+//  Alphanumeric characters include letters and numbers.
 
 public class ValidPalindrome {
     public static void main (String[] args) {
 
         String s = getString();
+        s = s.toLowerCase();
 
-        System.out.println(s);
+        String convertedText = letNumbersAndLetters(s);
+
+        palindromeVerification(convertedText);
 
     }
 
     public static String getString() {
-        //        example for texts:
+        //        example for palindrome phrase:
         //        A man, a plan, a canal: Panama
-        //        race a car
 
+        System.out.print("Please print a text: ");
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
 
         return s;
     }
+
+    public static String letNumbersAndLetters(String s) {
+
+        String conventedText = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char currentChar = s.charAt(i);
+
+            if (Character.isDigit(currentChar) || Character.isLetter(currentChar)) {
+                conventedText += currentChar;
+            }
+        }
+        return conventedText;
+    }
+
+    public static Boolean palindromeVerification(String convertedText) {
+
+    }
+
 }
